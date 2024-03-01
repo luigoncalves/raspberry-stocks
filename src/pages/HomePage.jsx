ccc125296fea5689279da0edb875536f0798c800
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-03-01 21:03:23 +0000

    organized homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -55,9 +55,9 @@ function HomePage() {
 
   const getMainNews = async () => {
     try {
-      // const responseNews = await axios.get(
-      //   'https://api.marketaux.com/v1/news/all?limit=5&language=en&api_token=3uu1zfw8GpGWNpAICjDrL1Al3ZeawfvsHDwW5Wy5'
-      // );
+      const responseNews = await axios.get(
+        'https://api.marketaux.com/v1/news/all?limit=15&language=en&api_token=3uu1zfw8GpGWNpAICjDrL1Al3ZeawfvsHDwW5Wy5'
+      );
       console.log(responseNews.data.data);
       setNews(responseNews.data.data);
     } catch (error) {
@@ -67,15 +67,15 @@ function HomePage() {
 
   const getMainCommodities = async () => {
     try {
-      // const responseC1 = await axios.get(
-      //   'https://financialmodelingprep.com/api/v3/quote/NGUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
-      // );
-      // const responseC2 = await axios.get(
-      //   'https://financialmodelingprep.com/api/v3/quote/CLUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
-      // );
-      // const responseC3 = await axios.get(
-      //   'https://financialmodelingprep.com/api/v3/quote/GCUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
-      // );
+      const responseC1 = await axios.get(
+        'https://financialmodelingprep.com/api/v3/quote/NGUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
+      );
+      const responseC2 = await axios.get(
+        'https://financialmodelingprep.com/api/v3/quote/CLUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
+      );
+      const responseC3 = await axios.get(
+        'https://financialmodelingprep.com/api/v3/quote/GCUSD?apikey=bad27d0fe5c04662a21dd5d7ca55ba93'
+      );
 
       console.log(responseC1.data);
       setCommodities([responseC1.data, responseC2.data, responseC3.data]);
@@ -104,13 +104,122 @@ function HomePage() {
       fontWeight='bold'
     >
       <GridItem pl='2' bg='orange.300' area={'header'}>
-        Header
+        <Box
+          display='flex'
+          animation='carousel 20s linear infinite'
+          h='80px'
+          justifyContent='space-evenly'
+          alignItems='center'
+          borderWidth='2px'
+          borderColor='green.500'
+          overflowX='auto'
+          w='100vw'
+        >
+          {stocks.map(stock => {
+            return (
+              <Box
+                key={stock.symbol}
+                w='300px'
+                h='50px'
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                borderWidth='10px'
+                borderColor='blue.500'
+                color='gray.600'
+              >
+                <Text>
+                  {stock[0].symbol} {stock[0].price}
+                </Text>
+
+                {stock[0].changesPercentage > 0 ? (
+                  <Text color='green.500' marginLeft='5'>
+                    {`+ ${stock[0].changesPercentage}%`}
+                  </Text>
+                ) : (
+                  <Text color='red.500' marginLeft='5'>
+                    {` ${stock[0].changesPercentage}%`}
+                  </Text>
+                )}
+              </Box>
+            );
+          })}
+        </Box>
       </GridItem>
       <GridItem pl='2' bg='pink.300' area={'main'}>
-        Main
+        <Flex flexDirection='column'>
+          {news.map(singleNews => {
+            return (
+              <ReactRouterLink to={singleNews.url} key={singleNews.uuid}>
+                <Flex>
+                  <Image
+                    src={singleNews.image_url}
+                    fallbackSrc='/news_substitute.jpg'
+                    alt='news'
+                    width='200px'
+                    height='auto'
+                  />
+                  <Box
+                    display='flex'
+                    flexDirection='column'
+                    justifyContent='flex-start'
+                  >
+                    <Heading as='h3' size='md' noOfLines={1}>
+                      {singleNews.title}
+                    </Heading>
+                    <Text>{singleNews.description}</Text>
+                  </Box>
+                </Flex>
+                <Divider orientation='horizontal' />
+              </ReactRouterLink>
+            );
+          })}
+        </Flex>
       </GridItem>
       <GridItem pl='2' bg='green.300' area={'commod'}>
-        Commod
+        <Flex flexDirection='column' align='center'>
+          //{' '}
+          <Heading as='h6' size='md'>
+            // Commodities //{' '}
+          </Heading>
+          //{' '}
+          <Flex flexDirection='column'>
+            {commodities.map(commod => {
+              return (
+                <Box
+                  key={commod.symbol}
+                  width='200px'
+                  h='50px'
+                  display='flex'
+                  alignItems='center'
+                  justifyContent='space'
+                  borderWidth='2px'
+                  borderColor='blue.500'
+                  color='gray.600'
+                  marginTop='5'
+                >
+                  <Text>
+                    {commod[0].symbol} {commod[0].price}
+                  </Text>
+                  <Spacer />
+                  {commod[0].changesPercentage > 0 ? (
+                    <Text color='green.500' marginLeft='5'>
+                      {`+ ${(
+                        Math.round(commod[0].changesPercentage * 100) / 100
+                      ).toFixed(3)}%`}
+                    </Text>
+                  ) : (
+                    <Text color='red.500' marginLeft='5'>
+                      {` ${(
+                        Math.round(commod[0].changesPercentage * 100) / 100
+                      ).toFixed(3)}%`}
+                    </Text>
+                  )}
+                </Box>
+              );
+            })}
+          </Flex>
+        </Flex>
       </GridItem>
     </Grid>
 
